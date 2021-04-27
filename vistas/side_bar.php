0f63afa43b33b9bd33550d122fb95d666b70fed9
OscarGonzalez14 <oscargonzalez815@gmail.com>
2021-04-27 16:50:17 -0600

    customizacion Plantilla

---
+++ vistas/side_bar.php
@@ -0,0 +1,72 @@
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <a class="brand-link">
+    </a>
+     <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+
+
+          <li class="nav-item">
+            <a class="nav-link" style="color: white">
+              <i class="nav-icon fas fa-file"></i>
+              <p>Ordenes</p>
+            </a>
+          </li>
+
+          <li class="nav-item">
+            <a href="#" class="nav-link" style="color: white">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Inventario
+                <i class="fas fa-angle-left right"></i>
+                <span class="badge badge-info right">6</span>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/layout/top-nav.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Adquisiciones</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/top-nav-sidebar.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Existencias</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/boxed.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Requisiciones</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/fixed-sidebar.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Lentes rotos</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/fixed-sidebar-custom.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Lentes</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/layout/fixed-topnav.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Insumos</p>
+                </a>
+              </li>
+             
+            </ul>
+          </li>
+
+          
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
